e65acd5fd7ffc2e5a2d6299147b6493d8b672471
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2024-11-17 09:07:11 +0100

    added time stamp

--- models/User.js
+++ models/User.js
@@ -2,12 +2,17 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  paid: { type: Boolean, default: false },
-});
-
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    paid: { type: Boolean, default: false },
+    cash: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("User", userSchema);
